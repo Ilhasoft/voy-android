453b357180585269365dd86b163b02b89d19cfe4
Erick Jones <erick.jones@ilhasoft.com.br>
2017-11-24 10:07:42 -0300

    Implementing SplashActivity to handle its layout and behaviors
    Signed-off-by: Erick Jones <erick.jones@ilhasoft.com.br>

---
+++ app/src/main/kotlin/br/com/ilhasoft/voy/ui/splash/SplashActivity.kt
@@ -0,0 +1,14 @@
+package br.com.ilhasoft.voy.ui.splash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import br.com.ilhasoft.voy.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+    }
+}
