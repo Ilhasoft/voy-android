71c75f22b0eddca1dcb61d90e7aa7d44b7dba9a0
Lucas Barros <lucas.barros@ilhasoft.com.br>
2017-11-24 19:12:23 -0300

    Selecting and showing images
    Signed-off-by: Lucas Barros <lucas.barros@ilhasoft.com.br>

--- app/src/main/kotlin/br/com/ilhasoft/voy/ui/addreport/medias/AddMediasContract.kt
+++ app/src/main/kotlin/br/com/ilhasoft/voy/ui/addreport/medias/AddMediasContract.kt
@@ -5,4 +5,6 @@ import br.com.ilhasoft.support.core.mvp.BasicView
 /**
  * Created by lucasbarros on 23/11/17.
  */
-interface AddMediasContract : BasicView
+interface AddMediasContract : BasicView {
+    fun getMedia()
+}
