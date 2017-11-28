2d5309d8afd612d76d4f492d8d31be3edd27e98e
Erick Jones <erick.jones@ilhasoft.com.br>
2017-11-28 14:16:01 -0300

    Implementing account screen layout, its basic structure and resources
    Signed-off-by: Erick Jones <erick.jones@ilhasoft.com.br>

---
+++ app/src/main/kotlin/br/com/ilhasoft/voy/ui/account/AccountActivity.kt
@@ -0,0 +1,45 @@
+package br.com.ilhasoft.voy.ui.account
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+
+
+import br.com.ilhasoft.voy.R
+import br.com.ilhasoft.voy.databinding.ActivityAccountBinding
+import br.com.ilhasoft.voy.ui.base.BaseActivity
+
+class AccountActivity : BaseActivity(), AccountContract {
+
+    private val binding: ActivityAccountBinding by lazy {
+        DataBindingUtil.setContentView<ActivityAccountBinding>(this, R.layout.activity_account)
+    }
+
+    private val presenter: AccountPresenter by lazy { AccountPresenter() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.run {
+            presenter = this@AccountActivity.presenter
+            toolbar?.apply {
+                buttonBack.setOnClickListener { onBackPressed() }
+            }
+        }
+        presenter.attachView(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        presenter.stop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.detachView()
+    }
+
+}
---
+++ app/src/main/kotlin/br/com/ilhasoft/voy/ui/account/AccountPresenter.kt
@@ -0,0 +1,5 @@
+package br.com.ilhasoft.voy.ui.account
+
+import br.com.ilhasoft.support.core.mvp.Presenter
+
+class AccountPresenter : Presenter<AccountContract>(AccountContract::class.java)
