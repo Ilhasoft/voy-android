45058181d259d5f287297144c1337378ffc87a15
Erick Jones <erick.jones@ilhasoft.com.br>
2017-11-29 16:52:38 -0300

    Thanks screen layout and its resources plus basic MVP structure

---
+++ app/src/main/kotlin/br/com/ilhasoft/voy/ui/addreport/thanks/ThanksActivity.kt
@@ -0,0 +1,45 @@
+package br.com.ilhasoft.voy.ui.addreport.thanks
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+
+
+import br.com.ilhasoft.voy.R
+import br.com.ilhasoft.voy.databinding.ActivityThanksBinding
+import br.com.ilhasoft.voy.ui.base.BaseActivity
+
+
+class ThanksActivity :BaseActivity(), ThanksContract {
+
+    private val binding: ActivityThanksBinding by lazy {
+        DataBindingUtil.setContentView<ActivityThanksBinding>(this, R.layout.activity_thanks)
+    }
+
+    private val presenter: ThanksPresenter by lazy { ThanksPresenter() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.run {
+            presenter = this@ThanksActivity.presenter
+        }
+        presenter.attachView(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        presenter.stop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.detachView()
+    }
+
+
+}
