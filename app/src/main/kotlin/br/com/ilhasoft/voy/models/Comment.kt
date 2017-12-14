3a00de00a4424d61e2f90959925a9df3ab7de4f1
Evandro Verdino <evandro.verdino@compelab.org>
2017-12-14 16:16:22 -0300

    Add base model to comments

---
+++ app/src/main/kotlin/br/com/ilhasoft/voy/models/Comment.kt
@@ -0,0 +1,8 @@
+package br.com.ilhasoft.voy.models
+
+/**
+ * Created by developer on 14/12/17.
+ */
+data class Comment(var username: String, var createdAt: String, var description: String) {
+    constructor() : this("", "", "")
+}
