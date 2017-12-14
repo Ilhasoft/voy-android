82742bc24a803522bf476114041fc8782dbeff6f
Evandro Verdino <evandro.verdino@compelab.org>
2017-12-14 17:54:46 -0300

    Add user model to comtemplate view

--- app/src/main/kotlin/br/com/ilhasoft/voy/models/Comment.kt
+++ app/src/main/kotlin/br/com/ilhasoft/voy/models/Comment.kt
@@ -3,6 +3,6 @@ package br.com.ilhasoft.voy.models
 /**
  * Created by developer on 14/12/17.
  */
-data class Comment(var username: String, var createdAt: String, var description: String) {
-    constructor() : this("", "", "")
+data class Comment(var user: User, var createdAt: String, var description: String) {
+    constructor() : this(User(), "", "")
 }
