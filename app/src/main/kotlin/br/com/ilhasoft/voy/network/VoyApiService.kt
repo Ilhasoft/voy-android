2d95cd14566ab0cc626b08e66ea6dcce2327b6eb
Evandro Verdino <evandro.verdino@compelab.org>
2018-01-05 14:06:54 -0300

    Add unfinished service

---
+++ app/src/main/kotlin/br/com/ilhasoft/voy/network/VoyApiService.kt
@@ -0,0 +1,45 @@
+package br.com.ilhasoft.voy.network
+
+import android.content.Context
+import br.com.ilhasoft.voy.R
+import br.com.ilhasoft.voy.models.User
+import io.reactivex.Flowable
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+/**
+ * Created by developer on 03/01/18.
+ */
+interface VoyApiService {
+
+    companion object {
+        fun create(context: Context): VoyApiService {
+            val networkInterceptor: HttpLoggingInterceptor =
+                    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val retrofit = Retrofit.Builder()
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(context.getString(R.string.server_url))
+                    .client(OkHttpClient.Builder()
+                            .addNetworkInterceptor(networkInterceptor)
+                            .build())
+                    .build()
+            return retrofit.create(VoyApiService::class.java)
+        }
+    }
+
+    @FormUrlEncoded
+    @GET("/api/users/")
+    fun getUsers(): Flowable<List<User>>
+
+    @FormUrlEncoded
+    @GET("/api/users/{id}/")
+    fun getUser(@Path("id") userId: String): Flowable<User>
+
+}
