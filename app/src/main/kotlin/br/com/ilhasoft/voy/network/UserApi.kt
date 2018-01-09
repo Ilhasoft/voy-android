d1f5f4fe1526c71cedc0467a1b6251484415a2f6
Evandro Verdino <evandro.verdino@compelab.org>
2018-01-09 15:24:36 -0300

    Remove unnecessary annotation and use correct observable

--- app/src/main/kotlin/br/com/ilhasoft/voy/network/UserApi.kt
+++ app/src/main/kotlin/br/com/ilhasoft/voy/network/UserApi.kt
@@ -1,15 +1,8 @@
 package br.com.ilhasoft.voy.network
 
-import android.content.Context
 import br.com.ilhasoft.voy.models.User
-import br.com.ilhasoft.voy.shared.Constants
 import io.reactivex.Flowable
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.FormUrlEncoded
+import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -18,12 +11,10 @@ import retrofit2.http.Path
  */
 interface UserApi {
 
-    @FormUrlEncoded
     @GET("/api/users/")
     fun getUsers(): Flowable<List<User>>
 
-    @FormUrlEncoded
     @GET("/api/users/{id}/")
-    fun getUser(@Path("id") userId: String): Flowable<User>
+    fun getUser(@Path("id") userId: String): Single<User>
 
 }
