b270ccce32761d2f73d9dda12ced353b9fa3e4ff
Lucas Barros <lucas.barros@ilhasoft.com.br>
2018-01-11 10:33:11 -0300

    Cleaning tests code

--- app/src/main/kotlin/br/com/ilhasoft/voy/ui/home/HomePresenter.kt
+++ app/src/main/kotlin/br/com/ilhasoft/voy/ui/home/HomePresenter.kt
@@ -3,17 +3,10 @@ package br.com.ilhasoft.voy.ui.home
 import br.com.ilhasoft.support.core.mvp.Presenter
 import br.com.ilhasoft.voy.models.Map
 import br.com.ilhasoft.voy.models.Notification
-import br.com.ilhasoft.voy.network.comments.CommentsService
-import br.com.ilhasoft.voy.shared.rx.RxHelper
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import timber.log.Timber
-import java.sql.Time
 
 class HomePresenter : Presenter<HomeContract>(HomeContract::class.java) {
 
     private var selectedMap: Map? = null
-    private val srv = CommentsService()
 
     fun onClickMyAccount() {
         view.navigateToMyAccount()
@@ -24,18 +17,7 @@ class HomePresenter : Presenter<HomeContract>(HomeContract::class.java) {
     }
 
     fun onClickNotifications() {
-
-        srv.deleteComment(1686)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnComplete { Timber.e("SUCCESS1") }
-                .subscribe({
-                    Timber.e("SUCCESS")
-                }, {
-                    Timber.e(it)
-                })
-
-//        view.showNotifications()
+        view.showNotifications()
     }
 
     fun onClickHeaderNavView() {
