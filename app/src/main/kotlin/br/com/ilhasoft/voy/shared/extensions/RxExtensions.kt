b4229a300e0c81600fb96d0be2cdc0018fc70cbb
Felipe <felipe.oliveira@ilhasoft.com.br>
2018-02-01 11:31:30 -0300

    Add more types to rx extensions

--- app/src/main/kotlin/br/com/ilhasoft/voy/shared/extensions/RxExtensions.kt
+++ app/src/main/kotlin/br/com/ilhasoft/voy/shared/extensions/RxExtensions.kt
@@ -3,6 +3,8 @@ package br.com.ilhasoft.voy.shared.extensions
 import br.com.ilhasoft.voy.ui.base.LoadView
 import io.reactivex.Completable
 import io.reactivex.Flowable
+import io.reactivex.Maybe
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
@@ -34,6 +36,38 @@ fun Completable.fromIoToMainThread(): Completable {
 }
 
 fun Completable.loadControl(loadView: LoadView): Completable {
+    return compose{ upstream ->
+        upstream
+                .doOnSubscribe { loadView.showLoad() }
+                .doAfterTerminate { loadView.dismissLoad() }
+    }
+}
+
+fun <T> Single<T>.fromIoToMainThread(): Single<T> {
+    return compose{ upstream ->
+        upstream
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+    }
+}
+
+fun <T> Single<T>.loadControl(loadView: LoadView): Single<T> {
+    return compose{ upstream ->
+        upstream
+                .doOnSubscribe { loadView.showLoad() }
+                .doAfterTerminate { loadView.dismissLoad() }
+    }
+}
+
+fun <T> Maybe<T>.fromIoToMainThread(): Maybe<T> {
+    return compose{ upstream ->
+        upstream
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+    }
+}
+
+fun <T> Maybe<T>.loadControl(loadView: LoadView): Maybe<T> {
     return compose{ upstream ->
         upstream
                 .doOnSubscribe { loadView.showLoad() }
