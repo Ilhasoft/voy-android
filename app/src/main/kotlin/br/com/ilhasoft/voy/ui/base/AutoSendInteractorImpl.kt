c47faa15ace8c5dd62956b15cf0d9e5b8d237b41
Lucas Barros <lucas.barros@ilhasoft.com.br>
2018-02-21 12:03:48 -0300

    Remove resend action on popup menu

--- app/src/main/kotlin/br/com/ilhasoft/voy/ui/base/AutoSendInteractorImpl.kt
+++ app/src/main/kotlin/br/com/ilhasoft/voy/ui/base/AutoSendInteractorImpl.kt
@@ -1,10 +1,12 @@
 package br.com.ilhasoft.voy.ui.base
 
 import br.com.ilhasoft.voy.db.report.ReportDbHelper
+import br.com.ilhasoft.voy.models.Report
 import br.com.ilhasoft.voy.network.reports.ReportService
 import br.com.ilhasoft.voy.shared.extensions.onMainThread
 import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
+import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
 import java.io.File
@@ -26,19 +28,33 @@ class AutoSendInteractorImpl : AutoSendInteractor {
     override fun sendPendingReports() {
         if (!sendingPendingReports) {
             getFromDb().observeOn(Schedulers.newThread())
-                    .doOnSubscribe { sendingPendingReports = true }
-                    .doOnTerminate { sendingPendingReports = false }
-                    .flatMap {
-                        Flowable.fromIterable(it)
-                    }
-                    .flatMap {
-                        reportService.saveReport(it.theme, it.location!!, it.description, it.name,
-                                it.tags, it.urls, it.files.map { File(it.file) }).toFlowable(BackpressureStrategy.DROP)
-                                .doOnComplete { reportDbHelper.removeReport(it.internalId!!) }
-                    }
-                    .subscribe({}, {
-                        Timber.e(it)
-                    })
+                .doOnSubscribe { sendingPendingReports = true }
+                .doOnTerminate { sendingPendingReports = false }
+                .flatMap {
+                    Flowable.fromIterable(it)
+                }
+                .flatMap {
+                    resendReport(it)
+                        //TODO: See other possibilities for backpressure strategy
+                        .toFlowable(BackpressureStrategy.DROP)
+                }
+                .subscribe({}, {
+                    Timber.e(it)
+                })
         }
+    }
+
+    private fun resendReport(report: Report): Observable<Report> {
+        val files = report.files.map { File(it.file) }
+        return reportService.saveReport(
+            report.theme,
+            report.location!!,
+            report.description,
+            report.name,
+            report.tags,
+            report.urls,
+            files
+        )
+            .doOnComplete { reportDbHelper.removeReport(report.internalId!!) }
     }
 }
--- app/src/main/kotlin/br/com/ilhasoft/voy/ui/report/fragment/ReportInteractorImpl.kt
+++ app/src/main/kotlin/br/com/ilhasoft/voy/ui/report/fragment/ReportInteractorImpl.kt
@@ -7,8 +7,6 @@ import br.com.ilhasoft.voy.network.reports.ReportService
 import br.com.ilhasoft.voy.shared.extensions.fromIoToMainThread
 import br.com.ilhasoft.voy.shared.extensions.onMainThread
 import io.reactivex.Flowable
-import io.reactivex.Observable
-import java.io.File
 
 /**
  * Created by lucasbarros on 09/02/18.
@@ -18,7 +16,13 @@ class ReportInteractorImpl(private val status: Int) : ReportInteractor {
     private val reportService by lazy { ReportService() }
     private val reportDbHelper by lazy { ReportDbHelper() }
 
-    override fun getReports(page: Int?, pageSize: Int?, theme: Int?, mapper: Int?, status: Int?): Flowable<List<Report>> {
+    override fun getReports(
+        page: Int?,
+        pageSize: Int?,
+        theme: Int?,
+        mapper: Int?,
+        status: Int?
+    ): Flowable<List<Report>> {
         return if (ConnectivityManager.isConnected()) {
             if (this@ReportInteractorImpl.status == ReportFragment.PENDING_STATUS) {
                 Flowable.merge(getFromServer(page, pageSize, theme, mapper, status), getFromDb())
@@ -36,17 +40,22 @@ class ReportInteractorImpl(private val status: Int) : ReportInteractor {
 
     private fun getFromDb() = reportDbHelper.getReports().onMainThread()
 
-    private fun getFromServer(page: Int?, pageSize: Int?, theme: Int?, mapper: Int?, status: Int?): Flowable<List<Report>> {
-        return reportService.getReports(page = page, page_size = pageSize, theme = theme, mapper = mapper, status = status)
-                .fromIoToMainThread()
-                .map { it.results }.toFlowable()
+    private fun getFromServer(
+        page: Int?,
+        pageSize: Int?,
+        theme: Int?,
+        mapper: Int?,
+        status: Int?
+    ): Flowable<List<Report>> {
+        return reportService.getReports(
+            page = page,
+            page_size = pageSize,
+            theme = theme,
+            mapper = mapper,
+            status = status
+        )
+            .fromIoToMainThread()
+            .map { it.results }.toFlowable()
     }
 
-    override fun resendReport(report: Report): Observable<Report> {
-        val files = report.files.map { File(it.file) }
-        return reportService.saveReport(report.theme, report.location!!, report.description,
-                report.name, report.tags, report.urls, files)
-                .doOnComplete { reportDbHelper.removeReport(report.internalId!!) }
-                .map { it }
-    }
 }
