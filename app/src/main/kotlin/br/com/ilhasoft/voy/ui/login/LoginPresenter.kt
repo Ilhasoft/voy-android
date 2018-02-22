e348e805769903b90d0c2487e89ccf8aa1e87af2
Allan Lima <allancslima@gmail.com>
2018-02-22 17:00:16 -0300

    Fix successful login message

--- app/src/main/kotlin/br/com/ilhasoft/voy/ui/login/LoginPresenter.kt
+++ app/src/main/kotlin/br/com/ilhasoft/voy/ui/login/LoginPresenter.kt
@@ -9,7 +9,9 @@ import br.com.ilhasoft.voy.network.BaseFactory
 import br.com.ilhasoft.voy.network.authorization.AuthorizationService
 import br.com.ilhasoft.voy.network.users.UserService
 import br.com.ilhasoft.voy.shared.helpers.RxHelper
+import io.reactivex.android.schedulers.AndroidSchedulers
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class LoginPresenter(private val preferences: Preferences) : Presenter<LoginContract>(LoginContract::class.java) {
 
@@ -33,6 +35,12 @@ class LoginPresenter(private val preferences: Preferences) : Presenter<LoginCont
                     })
                     .concatMap { userService.getUser() }
                     .compose(RxHelper.defaultFlowableSchedulers())
+                    .doOnNext {
+                        if (it.isNotEmpty() && it.first().isMapper)
+                            view.showMessage(R.string.login_success)
+                    }
+                    .delay(1, TimeUnit.SECONDS)
+                    .observeOn(AndroidSchedulers.mainThread())
                     .subscribe({
                         if (it.isNotEmpty() && it.first().isMapper) {
                             preferences.put(User.ID, it.first().id)
