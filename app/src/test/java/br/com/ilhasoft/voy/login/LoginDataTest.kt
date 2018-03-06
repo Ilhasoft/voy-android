be292ceb4bc25c3fe06ce403f52d7c80333bd482
Evandro Verdino <evandro.verdino@compelab.org>
2018-03-06 18:59:24 -0300

    Add timeout data test

--- app/src/test/java/br/com/ilhasoft/voy/login/LoginDataTest.kt
+++ app/src/test/java/br/com/ilhasoft/voy/login/LoginDataTest.kt
@@ -1,5 +1,6 @@
 package br.com.ilhasoft.voy.login
 
+import br.com.ilhasoft.voy.extensions.emitFlowableError
 import br.com.ilhasoft.voy.models.Credentials
 import br.com.ilhasoft.voy.network.authorization.AuthorizationDataSource
 import br.com.ilhasoft.voy.network.authorization.AuthorizationRepository
@@ -10,6 +11,7 @@ import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
+import java.util.concurrent.TimeoutException
 
 /**
  * Created by developer on 06/03/18.
@@ -18,7 +20,7 @@ class LoginDataTest {
 
     @Mock
     private lateinit var dataSource: AuthorizationDataSource
-    private lateinit var authorizationRepository: AuthorizationRepository
+    private lateinit var repository: AuthorizationRepository
 
     private val validCredentials = Credentials("pirralho", "123456")
     private val invalidCredentials = Credentials("unicef", "Unic@@f")
@@ -27,15 +29,15 @@ class LoginDataTest {
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
-        authorizationRepository = AuthorizationRepository(dataSource)
+        repository = AuthorizationRepository(dataSource)
     }
 
     @Test
     fun shouldMakeLoginWhenValidCredentials() {
-        `when`(dataSource.loginWithCredentials(validCredentials))
+        `when`(getLoginRef(validCredentials))
                 .thenReturn(Flowable.just(AuthorizationResponse(token)))
 
-        authorizationRepository.loginWithCredentials(validCredentials)
+        setupLoginImpl(validCredentials)
                 .test()
                 .assertSubscribed()
                 .assertNoErrors()
@@ -45,15 +47,30 @@ class LoginDataTest {
 
     @Test
     fun shouldNotMakeLoginWhenInvalidCredentials() {
-        `when`(dataSource.loginWithCredentials(invalidCredentials))
+        `when`(getLoginRef(invalidCredentials))
                 .thenReturn(Flowable.just(AuthorizationResponse("")))
 
-        authorizationRepository.loginWithCredentials(invalidCredentials)
+        setupLoginImpl(invalidCredentials)
                 .test()
                 .assertSubscribed()
                 .assertNoErrors()
                 .assertComplete()
                 .assertValue { (token) -> token.isEmpty() }
     }
+
+    @Test
+    fun shouldThrowExceptionWhenMakeLoginWithTimeout() {
+        `when`(getLoginRef(validCredentials))
+                .thenReturn(emitFlowableError(TimeoutException()))
+
+        setupLoginImpl(validCredentials)
+                .test()
+                .assertSubscribed()
+                .assertError { it is TimeoutException }
+    }
+
+    private fun getLoginRef(credentials: Credentials) = dataSource.loginWithCredentials(credentials)
+
+    private fun setupLoginImpl(credentials: Credentials) = repository.loginWithCredentials(credentials)
 
 }
