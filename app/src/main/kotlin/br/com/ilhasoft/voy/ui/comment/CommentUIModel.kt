14484972a64b0626d1ee83cec41932a1d198df9b
Felipe <felipe.oliveira@ilhasoft.com.br>
2018-03-08 18:34:34 -0300

    Test comment mapper

--- app/src/main/kotlin/br/com/ilhasoft/voy/ui/comment/CommentUIModel.kt
+++ app/src/main/kotlin/br/com/ilhasoft/voy/ui/comment/CommentUIModel.kt
@@ -4,11 +4,11 @@ package br.com.ilhasoft.voy.ui.comment
  * Created by felipe on 31/01/18.
  */
 data class CommentUIModel(
-        val commentId: Int,
-        val reportId: Int,
-        val userId: Int,
-        val createdBy: String,
-        val body: String,
-        val createdOn: String,
-        val avatar: String
+    val commentId: Int,
+    val reportId: Int,
+    val userId: Int,
+    val createdBy: String,
+    val body: String,
+    val createdOn: String,
+    val avatar: String
 )
