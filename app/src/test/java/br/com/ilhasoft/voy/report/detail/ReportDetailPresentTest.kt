0367cee6476cc9ba2a5f22d25bf4544c921967a6
Evandro Verdino <evandro.verdino@compelab.org>
2018-03-09 15:23:24 -0300

    Add test to check report load

--- app/src/test/java/br/com/ilhasoft/voy/report/detail/ReportDetailPresentTest.kt
+++ app/src/test/java/br/com/ilhasoft/voy/report/detail/ReportDetailPresentTest.kt
@@ -1,19 +1,20 @@
 package br.com.ilhasoft.voy.report.detail
 
 import br.com.ilhasoft.voy.connectivity.CheckConnectionProvider
-import br.com.ilhasoft.voy.models.Indicator
-import br.com.ilhasoft.voy.models.Preferences
-import br.com.ilhasoft.voy.models.Report
+import br.com.ilhasoft.voy.models.*
 import br.com.ilhasoft.voy.network.reports.ReportDataSource
 import br.com.ilhasoft.voy.network.reports.ReportRepository
 import br.com.ilhasoft.voy.shared.schedulers.ImmediateScheduler
 import br.com.ilhasoft.voy.ui.report.detail.ReportDetailContract
 import br.com.ilhasoft.voy.ui.report.detail.ReportDetailPresenter
+import io.reactivex.Single
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
+import java.util.*
 
 /**
  * Created by developer on 08/03/18.
@@ -33,8 +34,10 @@ class ReportDetailPresentTest {
     private lateinit var view: ReportDetailContract
     private lateinit var presenter: ReportDetailPresenter
 
-    private val mockedReport = Report()
     private val mockedIndicator = Indicator()
+    private val mockedLocation = createMockedLocation()
+    private val mockedUser = createMockedUser()
+    private val mockedReport = createMockedReport()
 
     @Before
     fun setup() {
@@ -71,5 +74,61 @@ class ReportDetailPresentTest {
 
         verify(view).swapPage(mockedIndicator)
     }
+
+    @Test
+    fun shouldLoadReportWithConnection() {
+        `when`(connectionProvider.hasConnection())
+                .thenReturn(true)
+
+        `when`(preferences.getInt(User.ID))
+                .thenReturn(1)
+
+        `when`(dataSource.getReport(id = mockedReport.id, theme = mockedReport.theme,
+                mapper = preferences.getInt(User.ID), status = mockedReport.status))
+                .thenReturn(Single.just(mockedReport))
+
+        presenter.loadReportData()
+
+        verify(view).showLoading()
+        verify(view).populateCarousel(presenter.getCarouselItems(mockedReport))
+        verify(view).populateIndicator(presenter.getIndicators(mockedReport))
+        verify(view).showReportData(mockedReport)
+        verify(view).dismissLoading()
+    }
+
+    private fun createMockedLocation() = Location("GPS", arrayListOf(1.2, 2.2))
+
+    private fun createMockedUser() = User(
+            1,
+            "firstName",
+            "lastName",
+            "language",
+            "avatar",
+            "username",
+            "email",
+            true,
+            true,
+            "password"
+    )
+
+    private fun createMockedReport() = Report(
+            1,
+            1,
+            mockedLocation,
+            true,
+            true,
+            true,
+            Date(),
+            "description",
+            "name",
+            mutableListOf(),
+            "themeColor",
+            mockedUser,
+            null,
+            1,
+            mutableListOf(),
+            mutableListOf(),
+            "lastNotification"
+    )
 
 }
