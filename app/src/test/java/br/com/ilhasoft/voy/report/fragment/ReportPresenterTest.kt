090c39e3a64c725626a6bf3eb891e7511580540f
Evandro Verdino <evandro.verdino@compelab.org>
2018-03-13 15:14:22 -0300

    Add tests to cover report fragment presenter

---
+++ app/src/test/java/br/com/ilhasoft/voy/report/fragment/ReportPresenterTest.kt
@@ -0,0 +1,94 @@
+package br.com.ilhasoft.voy.report.fragment
+
+import br.com.ilhasoft.voy.models.Location
+import br.com.ilhasoft.voy.models.Preferences
+import br.com.ilhasoft.voy.models.Report
+import br.com.ilhasoft.voy.models.User
+import br.com.ilhasoft.voy.ui.report.fragment.ReportContract
+import br.com.ilhasoft.voy.ui.report.fragment.ReportPresenter
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+import java.util.*
+
+/**
+ * Created by developer on 13/03/18.
+ */
+class ReportPresenterTest {
+
+    @Mock
+    private lateinit var preferences: Preferences
+
+    @Mock
+    private lateinit var view: ReportContract
+    private lateinit var presenter: ReportPresenter
+
+    private val mockedLocation = createMockedLocation()
+    private val mockedUser = createMockedUser()
+    private val mockedReport = createMockedReport()
+
+    private val mockedUserAvatar = "http://voy-dev.ilhasoft.mobi/media/users/avatars/group-21.png"
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        presenter = ReportPresenter(preferences)
+        presenter.attachView(view)
+    }
+
+    @Test
+    fun shouldNavigateToShowReportDetail() {
+        presenter.navigateToReportDetail(mockedReport)
+
+        verify(view).navigateToReportDetail(mockedReport)
+    }
+
+    @Test
+    fun shouldGetAvatarReference() {
+        `when`(preferences.getString(User.AVATAR)).thenReturn(mockedUserAvatar)
+
+        presenter.getAvatarPositionFromPreferences()
+
+        assertEquals(preferences.getString(User.AVATAR), mockedUserAvatar)
+    }
+
+    private fun createMockedLocation() = Location("GPS", arrayListOf(1.2, 2.2))
+
+    private fun createMockedUser() = User(
+            1,
+            "firstName",
+            "lastName",
+            "language",
+            "avatar",
+            "username",
+            "email",
+            true,
+            true,
+            "password"
+    )
+
+    private fun createMockedReport() = Report(
+            1,
+            1,
+            mockedLocation,
+            true,
+            true,
+            true,
+            Date(),
+            "description",
+            "name",
+            mutableListOf(),
+            "themeColor",
+            mockedUser,
+            null,
+            1,
+            mutableListOf(),
+            mutableListOf(),
+            "lastNotification"
+    )
+
+}
